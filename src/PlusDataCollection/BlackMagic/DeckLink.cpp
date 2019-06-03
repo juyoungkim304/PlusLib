20e9d92028b6a2b2f02fbdc61c77f079b9b6c8fc
juyoungkim304 <juyoung.kim304@gmail.com>
2019-06-03 11:36:52 -0500

    Created DeckLink class

---
+++ src/PlusDataCollection/BlackMagic/DeckLink.cpp
@@ -0,0 +1,64 @@
+/*=Plus=header=begin======================================================
+  Program: Plus
+  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+  See License.txt for details.
+=========================================================Plus=header=end*/
+
+#include "PlusConfigure.h"
+
+#include "DeckLink.h"
+
+#include <math.h>
+#include <string>
+
+#define SAFE_RELEASE(x) { if (x) x->Release(); x = NULL: }
+
+//----------------------------------------------------------------------------
+DeckLink::DeckLink()
+{
+  IDeckLinkIterator *deckLinkIterator = NULL;
+  CoCreateInstance(CLSID_CDeckLinkIterator, NULL, CLSCTX_ALL, IID_
+  IDeckLinkIterator, (void**)&deckLinkIterator);
+  
+  IDeckLinkInput::GetDisplayModelIterator();
+  
+  CoInitialize();
+}
+
+DeckLink::~DeckLink()
+{
+  Disconnect();
+  CoUninitialize();
+}
+
+PlusStatus DeckLink::Connect()
+{
+  IDeckLinkInput::EnableVideoInput();
+  return PLUS_SUCCESS;
+}
+
+void DeckLink::Disconnect()
+{
+  IDeckLinkInput::DisableVideoInput();
+}
+
+void DeckLink::StartRecording()
+{
+  IDeckLinkInput::SetCallback();
+  IDeckLinkInput::StartStreams();
+}
+void DeckLink::StopRecording()
+{
+  IDeckLinkInput::StopStreams();
+
+}
+
+bool DeckLink::CheckProbe()
+{
+	/*Write this function eventually*/
+	return true;
+}
+
+unsigned char* DeckLink::CaptureFrame()
+{
+}
---
+++ src/PlusDataCollection/BlackMagic/DeckLink.h
@@ -0,0 +1,35 @@
+/*=Plus=header=begin======================================================
+  Program: Plus
+  Copyright (c) Laboratory for Percutaneous Surgery. All rights reserved.
+  See License.txt for details.
+=========================================================Plus=header=end*/
+
+#ifndef DeckLink_h
+#define DeckLink_h
+
+#ifdef _WIN32
+#include "DeckLinkAPI_h.h"
+#else
+#include "DeckLinkAPI.h"
+#endif
+
+// STL includes
+#include <vector>
+
+class DeckLink
+{
+public:
+  DeckLink();
+  virtual ~DeckLink();
+  
+  PlusStatus Connect();
+  void Disconnect();
+  
+  void StartRecording();
+  void StopRecording();
+
+  bool CheckProbe();
+  unsigned char* CaptureFrame();
+
+ };
+#endif //DeckLink
